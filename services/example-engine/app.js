16bb87b398ea5d1f7e0907ee33d196c375580b83
shyam-habarakada <shyam.habarakada@gmail.com>
2016-05-15 15:25:09 -0700

    more cleanup of the code

--- services/example-engine/app.js
+++ services/example-engine/app.js
@@ -1,29 +1,16 @@
-//process.env.NODE_CONFIG_DIR = process.cwd() + '/config/env';
-
 var config = require('config');
-console.log(config);
-
-/* Also useful to only get Service-specific config:
-  var config = require('config').get('engine');
-*/
 
 function doWork() {
-  //console.log('staying alive. keeping busy.');
+  console.log('staying alive. keeping busy.');
 }
 
-var intId = setInterval(doWork,5000);
-
-
+var runLoop = setInterval(doWork,5000);
 
 process.on('SIGINT', function() {
-  // My process has received a SIGINT signal
-  // Meaning PM2 is now trying to stop the process
+  clearInterval(runLoop);
 
-  // So I can clean some stuff before the final stop
-  clearInterval(intId);
-
-  setTimeout(function() {
-    // 300ms later the process kill it self to allow a restart
-    //process.exit(0);
-  }, 300);
+  // setTimeout(function() {
+  //  // TODO What is the corect way to cleanup, then exit on SIGINT?
+  //  process.exit(0);
+  // }, 300);
 });
