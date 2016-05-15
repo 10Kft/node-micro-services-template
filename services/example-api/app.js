3424c185dd30cd59bc53f307aebbd75f50573de3
Skylar Graika <skygraika127@gmail.com>
2016-05-14 20:20:13 -0700

    Integrated config module and setup test environment resources. This module loads the appropriate config file based on environment. Aslo configured the file to overide and add to configuration for variables found in  environment.

--- services/example-api/app.js
+++ services/example-api/app.js
@@ -1,11 +1,20 @@
+process.env.NODE_CONFIG_DIR = process.cwd() + '/config/env';
+
+var config = require('config');
+console.log(config);
+
+/* Also useful to only get Service-specific config:
+  var config = require('config').get('api');
+*/
+
 var express = require('express');
 var app = express();
-
+var port = config.get('api.port');
 app.get('/', function (req, res) {
   console.log('processing request /');
   res.send('Hello World!');
 });
 
-app.listen(8001, function () {
-  console.log('Example app listening on port 8001');
+app.listen(port, function () {
+  console.log('Example app listening on port', port);
 });
--- services/example-engine/app.js
+++ services/example-engine/app.js
@@ -1,6 +1,14 @@
+process.env.NODE_CONFIG_DIR = process.cwd() + '/config/env';
+
+var config = require('config');
+console.log(config);
+
+/* Also useful to only get Service-specific config:
+  var config = require('config').get('engine');
+*/
 
 function doWork() {
-  console.log('staying alive. keeping busy.');
+  //console.log('staying alive. keeping busy.');
 }
 
 setInterval(doWork,5000);
