5bba9b65cc3d315f4a714923d9c6c23093412f4c
Shyam Habarakada <shyam.habarakada@gmail.com>
2016-05-15 17:06:03 -0700

    add a simple local module (or library)
    @swordfish444 CR please.
    
    We still need a good way to avoid using the `../../path` stuff in
    require statements. This is tbd, but this commit how we might implement
    local modules that have shared functionality across multiple service
    apps.

--- services/example-api/app.js
+++ services/example-api/app.js
@@ -1,11 +1,13 @@
 var config = require('config');
-
 var express = require('express');
+var greetings = require('../../lib/greetings.js').greetings;
+
 var app = express();
 var port = config.get('api.port');
+
 app.get('/', function (req, res) {
   console.log('processing request /');
-  res.send('Hello World!');
+  res.send(greetings.helloWorld());
 });
 
 app.listen(port, function () {
